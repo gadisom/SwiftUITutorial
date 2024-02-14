cfe2c23ef97ee638205e50a07658b0f08e8f48ad
iOS-jeongwon <kim.jeongwon@cashwalk.io>
2024-02-14 17:28:21 +0900

    Initial Commit

---
+++ SwiftUITutorial/SwiftUITutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITutorialApp.swift
+//  SwiftUITutorial
+//
+//  Created by 김정원 on 2/14/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
