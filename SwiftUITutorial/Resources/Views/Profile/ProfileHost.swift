4246ff9736b3332431be27ee0cb684eaa8586980
iOS-jeongwon <kim.jeongwon@cashwalk.io>
2024-02-18 18:14:06 +0900

    feat: App design

---
+++ SwiftUITutorial/Resources/Views/Profile/ProfileHost.swift
@@ -0,0 +1,49 @@
+//
+//  ProfileHost.swift
+//  SwiftUITutorial
+//
+//  Created by 김정원 on 2/18/24.
+//
+
+import SwiftUI
+
+
+struct ProfileHost: View {
+    @Environment(\.editMode) var editMode
+    @Environment(ModelData.self) var modelData
+    @State private var draftProfile = Profile.default
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 20) {
+            HStack {
+                if editMode?.wrappedValue == .active {
+                    Button("Cancel", role: .cancel) {
+                        draftProfile = modelData.profile
+                        editMode?.animation().wrappedValue = .inactive
+                    }
+                }
+                Spacer()
+                EditButton()
+            }
+            
+            if editMode?.wrappedValue == .inactive {
+                ProfileSummary(profile: modelData.profile)
+            } else {
+                ProfileEditor(profile: $draftProfile)
+                    .onAppear {
+                        draftProfile = modelData.profile
+                    }
+                    .onDisappear {
+                        modelData.profile = draftProfile
+                    }
+            }
+        }
+        .padding()
+    }
+}
+
+
+#Preview {
+    ProfileHost()
+        .environment(ModelData())
+}
