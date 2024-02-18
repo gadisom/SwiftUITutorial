dd3812a7b4bf8cf4957d2023d9634a59d3941b49
iOS-jeongwon <kim.jeongwon@cashwalk.io>
2024-02-18 15:35:35 +0900

    feat: Drawing paths

---
+++ SwiftUITutorial/Resources/Views/Badges/HexagonParameters.swift
@@ -0,0 +1,51 @@
+//
+//  HexagonParameters.swift
+//  SwiftUITutorial
+//
+//  Created by 김정원 on 2/17/24.
+//
+
+import CoreGraphics
+
+struct HexagonParameters {
+    struct Segment { // 3개의 점을 나타내는 segment 구조체를 정의하여 HexagonParameters 파일을 생성한다.
+        let line: CGPoint
+        let curve: CGPoint
+        let control: CGPoint
+    }
+
+    static let adjustment: CGFloat = 0.085 //모양을 조절하는 조정값을 추가한다.
+
+    static let segments = [ // 육각형의 여섯 변에 대한 각각의 데이터를 추가한다.
+        Segment(
+            line:    CGPoint(x: 0.60, y: 0.05),
+            curve:   CGPoint(x: 0.40, y: 0.05),
+            control: CGPoint(x: 0.50, y: 0.00)
+        ),
+        Segment(
+            line:    CGPoint(x: 0.05, y: 0.20 + adjustment),
+            curve:   CGPoint(x: 0.00, y: 0.30 + adjustment),
+            control: CGPoint(x: 0.00, y: 0.25 + adjustment)
+        ),
+        Segment(
+            line:    CGPoint(x: 0.00, y: 0.70 - adjustment),
+            curve:   CGPoint(x: 0.05, y: 0.80 - adjustment),
+            control: CGPoint(x: 0.00, y: 0.75 - adjustment)
+        ),
+        Segment(
+            line:    CGPoint(x: 0.40, y: 0.95),
+            curve:   CGPoint(x: 0.60, y: 0.95),
+            control: CGPoint(x: 0.50, y: 1.00)
+        ),
+        Segment(
+            line:    CGPoint(x: 0.95, y: 0.80 - adjustment),
+            curve:   CGPoint(x: 1.00, y: 0.70 - adjustment),
+            control: CGPoint(x: 1.00, y: 0.75 - adjustment)
+        ),
+        Segment(
+            line:    CGPoint(x: 1.00, y: 0.30 + adjustment),
+            curve:   CGPoint(x: 0.95, y: 0.20 + adjustment),
+            control: CGPoint(x: 1.00, y: 0.25 + adjustment)
+        )
+    ]
+}
---
+++ SwiftUITutorial/Resources/Views/Badges/RotatedBadgeSymbol.swift
@@ -0,0 +1,23 @@
+//
+//  RotatedBadgeSymbol.swift
+//  SwiftUITutorial
+//
+//  Created by 김정원 on 2/17/24.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+
+#Preview {
+    RotatedBadgeSymbol(angle: Angle(degrees: 5))
+}
